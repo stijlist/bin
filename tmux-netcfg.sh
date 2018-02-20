87e606d00eec3234e35ad68e2bf44dd1df0d52da
Bert Muthalaly <bert@somethingdoneright.net>
2018-02-20 13:15:17 -0500

    Change directory to ~/fuchsia before invoking fx commands.

--- tmux-netcfg.sh
+++ tmux-netcfg.sh
@@ -17,9 +17,9 @@ tmux -2 new-session -d -s "$SESSION"
 # tmux new-window -t $SESSION:1 -n "$SESSION"
 tmux split-window -h
 tmux select-pane -t 1
-tmux send-keys "cd ~/fuchsia; sudo ./netcfg.sh $IFACE" Enter
+tmux send-keys "cd ~/fuchsia; sudo $(which netcfg.sh) $IFACE" Enter
 tmux split-window -v
-tmux send-keys "fx log" Enter
+tmux send-keys "cd ~/fuchsia; fx log" Enter
 tmux split-window -v
-tmux send-keys "fx boot" Enter
+tmux send-keys "cd ~/fuchsia; fx boot" Enter
 tmux attach -t "$SESSION"
